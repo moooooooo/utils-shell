8eb6324714e2b19aead14f39edadee0c733d7340
moooooooo <peter.moore350@gmail.com>
2023-11-25 23:39:53 +1300

    added comment with orig repo url

--- PeterOpenCore-Boot.sh
+++ PeterOpenCore-Boot.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+# orig repo:
+# https://github.com/kholia/OSX-KVM.git
+
 # Special thanks to:
 # https://github.com/Leoyzen/KVM-Opencore
 # https://github.com/thenickdude/KVM-Opencore/
 # https://github.com/qemu/qemu/blob/master/docs/usb2.txt
 #
-# qemu-img create -f qcow2 mac_hdd_ng.img 128G
+# after running fetch-macOS-v2.py, do the next 2
+#
+# dmg2img -i BaseSystem.dmg BaseSystem.img
+# qemu-img create -f qcow2 mac_hdd_ng.img 750G
 #
 # echo 1 > /sys/module/kvm/parameters/ignore_msrs (this is required)
 
@@ -29,7 +35,8 @@ OVMF_DIR="."
 # shellcheck disable=SC2054
 args=(
   -enable-kvm -m "$ALLOCATED_RAM" -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,"$MY_OPTIONS"
-  -machine q35
+  # -machine q35
+  -machine ubuntu-q35
   -usb -device usb-kbd -device usb-tablet
   -smp "$CPU_THREADS",cores="$CPU_CORES",sockets="$CPU_SOCKETS"
   -device usb-ehci,id=ehci
@@ -52,12 +59,26 @@ args=(
   -drive id=InstallMedia,if=none,file="$REPO_PATH/BaseSystem.img",format=raw
   -drive id=MacHDD,if=none,file="$REPO_PATH/mac_hdd_ng.img",format=qcow2
   -device ide-hd,bus=sata.4,drive=MacHDD
+  # --------------------------------------------------------------------------------
+  # Peter - test img 
+  # --------------------------------------------------------------------------------
+  # -drive id=MacHDDPtr,if=none,file="/home/peter/src/mac_hdd_peter.img",format=qcow2
+  # -device ide-hd,bus=sata.5,drive=MacHDDPtr
+  # end peter test
+  # --------------------------------------------------------------------------------
   # Peter emily hdd
   # -device ide-hd,bus=sata.5,drive=Spinning
   # -drive id=Spinning,if=none,file="/dev/sda2",format=raw
   # end emily hdd
+  # --------------------------------------------------------------------------------
+  # Peter - Windows disk
+  # -device ide-hd,bus=sata.5,drive=Windows
+  # -drive id=Windows,if=none,file="/dev/sdc",format=raw
+  # end Windows disk
+  # --------------------------------------------------------------------------------
   # -netdev tap,id=net0,ifname=tap0,script=no,downscript=no -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:c9:18:27
-  -netdev user,id=net0 -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:c9:18:27
+  -netdev user,id=net0 -device vmxnet3,netdev=net0,id=net0,mac=52:54:00:c9:18:27
+  # -netdev user,id=net0 -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:c9:18:27
   # -netdev user,id=net0 -device vmxnet3,netdev=net0,id=net0,mac=52:54:00:c9:18:27  # Note: Use this line for High Sierra
   -monitor stdio
   -device vmware-svga,vgamem_mb=64
