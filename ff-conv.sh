04679009b323a0b70c8e6182ea32bbc0e36b05cd
unknown <peter.moore350@gmail.com>
2024-05-21 11:20:05 +1200

    Added history from batch processing

--- ff-conv.sh
+++ ff-conv.sh
@@ -19,3 +19,21 @@ ffmpeg -i in.ogg -map_metadata 0:s:0 out.mp3
 
 # from here:
 # https://ffmpeg.org//ffmpeg.html#Video-and-Audio-file-format-conversion
+#
+# Directory process etc
+#845  20/05/24 21:39:20 find  ~/Music/zzz/ -type f -name *.ogg > list-ogg.txt
+#846  20/05/24 21:39:28 cat list-ogg.txt
+#847  20/05/24 21:39:40 find  ~/Music/zzz/ -type f -name *.flac > list-flac.txt
+#848  20/05/24 21:39:47 cat list-flac.txt
+#849  20/05/24 21:39:58 rm list.txt
+#850  20/05/24 21:40:16 find  ~/Music/zzz/a_positive_life -type f -name *.ogg > list.txt
+#851  20/05/24 21:42:36 FILES="cat list.txt"
+#852  20/05/24 21:42:41 echo $FILES
+#853  20/05/24 21:42:49 FILES=`cat list.txt`
+#854  20/05/24 21:42:50 echo $FILES
+#855  20/05/24 21:43:13 for i in $FILES;do echo $i; done;
+#856  20/05/24 21:43:28 for i in $FILES;do echo file is $i; done;
+#857  20/05/24 21:43:44 for i in $FILES;do echo rpocessing  $i; done;
+#858  20/05/24 21:44:07 for i in $FILES;do echo rpocessing  $i; ./ffprobe.exe $i -export_all true; done;
+#859  20/05/24 21:45:54 for i in $FILES;do echo rpocessing  $i;  ./ffmpeg.exe  -stats -i "$i"  -map_metadata 0:s:0 -acodec alac "${i%.*}.m4a"; done;
+
